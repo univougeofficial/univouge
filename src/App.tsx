0dff9fdafcbd038b4284449278e05b00daf96b47
Dhanvesh Naga Sai <dhanveshnagasaia@voltacabs.com>
2025-12-30 20:05:46 +0530

    removed the courten

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Header from './components/Header';
 import Home from './components/Home';
@@ -10,34 +10,13 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Experience from './components/Experience';
 import FloatingActions from './components/FloatingActions';
-import CurtainReveal from './components/CurtainReveal';
 
 
 const App: React.FC = () => {
-  const [showCurtain, setShowCurtain] = useState(true);
-  const [hasSeenIntro, setHasSeenIntro] = useState(false);
-
-  // Check if user has already seen the intro in this session
-  useEffect(() => {
-    const seen = sessionStorage.getItem('univouge_intro_seen');
-    if (seen) {
-      setShowCurtain(false);
-      setHasSeenIntro(true);
-    }
-  }, []);
-
-  const handleCurtainComplete = () => {
-    setShowCurtain(false);
-    setHasSeenIntro(true);
-    sessionStorage.setItem('univouge_intro_seen', 'true');
-  };
 
   return (
     <div className="font-serif text-gray-800">
       {/* Curtain Reveal Animation */}
-      {showCurtain && !hasSeenIntro && (
-        <CurtainReveal onComplete={handleCurtainComplete} />
-      )}
 
       <Header />
       <FloatingActions />
